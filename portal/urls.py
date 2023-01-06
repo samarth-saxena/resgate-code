f2bcbf11af7c240635b8b19c1be5b7b56906bae9
Samarth Saxena <saxena.samarth02@gmail.com>
2023-01-06 12:51:00 +0530

    Added basic URL mappings

--- portal/urls.py
+++ portal/urls.py
@@ -3,5 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_hello, name='index'),
+    path('hello/', views.say_hello, name='index'),
+
+	path('',views.student_home, name='student_home'),
+	path('browse/',views.student_browse, name='student_browse'),
+
+	path('prof/',views.prof_home, name='prof_home'), # recommended applications
+	path('prof/students',views.prof_students, name='prof_students'), # current students
+	path('prof/projects',views.prof_projects, name='prof_projects'), # my openings, add new openings
+
+
 ]
--- portal/views.py
+++ portal/views.py
@@ -8,3 +8,18 @@ from django.http import HttpResponse
 
 def say_hello(request):
 	return render(request, 'hello.html', {'name':'Sam'})
+
+def student_home(request):
+	return render(request, 'students/stud_home.html')
+
+def student_browse(request):
+	return HttpResponse("Browse")
+
+def prof_home(request):
+	return HttpResponse("Prof home: View recommended and new applications")
+
+def prof_projects(request):
+	return HttpResponse("Listings of all current projects, and add new projects")
+
+def prof_students(request):
+	return HttpResponse("Listings of all current students working")
