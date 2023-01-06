8da8e44c45fdb09a464c82815bed4c04f5d447d2
Samiksha Modi <samikshamodi7@gmail.com>
2023-01-06 15:12:06 +0530

    choices added

--- portal/models.py
+++ portal/models.py
@@ -3,33 +3,72 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=60)
-    program = models.CharField(max_length=10) #btech, mtech, phd Make choices?
-    branch = models.CharField(max_length=10) #cse, csd, ece, csb,etc Make choices?
-    batch = models.IntegerField(default=0)
+
+    BTECH = 'B'
+    MTECH = 'M'
+    PHD = 'P'
+    PROGRAM_CHOICES = [
+        (BTECH, 'BTech'),
+        (MTECH, 'MTech'),
+        (PHD, 'PhD'),
+    ]
+    program = models.CharField(max_length=1,choices=PROGRAM_CHOICES,default=BTECH) 
+
+    CSE = 'CSE'
+    CSD = 'CSD'
+    CSB = 'CSB'
+    CSSS = 'CSSS'
+    CSAI = 'CSAI'
+    ECE = 'ECE'
+    EEE = 'EEE'
+    CB = 'CB'
+    BRANCH_CHOICES = [
+        (CSE, 'CSE'),
+        (CSD, 'CSD'),
+        (CSB, 'CSB'),
+        (CSSS, 'CSSS'),
+        (CSAI, 'CSAI'),
+        (ECE, 'ECE'),
+        (EEE, 'EEE'),
+        (CB, 'CB'),
+    ]
+    branch = models.CharField(max_length=4,choices=BRANCH_CHOICES,default=CSE) 
+
+    batch = models.IntegerField(default=2022)
     email = models.EmailField(max_length = 254)
     tags = models.JSONField()
     #Student_CV
+
+class StudentCV(models.Model):
+    file = models.FileField(upload_to='uploads/%Y/%m/%d/')
+
+class Lab(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(max_length=300)
+
+class Domain(models.Model):
+    title = models.CharField(max_length=255)
 
 class Professor(models.Model):
     name = models.CharField(max_length=60)
     email = models.EmailField(max_length = 254)
     #lab = Lab
-    website = models.URLField(max_length=300)
-
-class Lab(models.Model):
-    name = models.CharField(max_length=100)
-    website = models.URLField(max_length=300)
     #professor (one lab can have many prof, but one prof can have 1 lab only)
+    website = models.URLField(max_length=300)
 
 class Openings(models.Model):
     #lab = Lab (one lab can have many openings)
     #professor = Professor (one opening can only have many professor)
     #domain = Domain
     description = models.TextField()
-    status = models.CharField(max_length=10)    #open or close Make choices?
 
-class Domain(models.Model):
-    title = models.CharField(max_length=255)
+    OPEN = 'O'
+    CLOSED = 'C'
+    STATUS_CHOICES = [
+        (OPEN, 'Open'),
+        (CLOSED, 'Closed'),
+    ]
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES,default=OPEN)    
 
 class Application(models.Model):
     #student = Student
@@ -37,20 +76,16 @@ class Application(models.Model):
     openingDate = models.DateField()
     updateDate = models.DateField()
     #student_cv = Student_CV
-    status = models.CharField(max_length=10) #pending, rejected, accepted Make choices?
 
-class StudentCV(models.Model):
-    #student = Student
-    file = models.FileField(upload_to='uploads/%Y/%m/%d/')
-
-
-
-
-
-
-
+    PENDING = 'P'
+    REJECTED = 'R'
+    ACCEPTED = 'A'
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (REJECTED, 'Rejected'),
+        (ACCEPTED, 'Accepted'),
+    ]
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES,default=PENDING)
 
 
 
-    def __str__(self):
-        return self.address1 + self.address2 + self.country
