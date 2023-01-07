ca35a5e01d73d24c9a4450e9d4e637d5abaf1602
Samarth Saxena <saxena.samarth02@gmail.com>
2023-01-08 00:20:58 +0530

    Remapped student URLs

--- portal/urls.py
+++ portal/urls.py
@@ -6,12 +6,11 @@ urlpatterns = [
 	path('login/', views.login_view, name='login'),
 	path('signup/', views.signup_view, name='signup'),
 	path('logout/', views.logout_view, name='logout'),
-
-
-	path('',views.student_home, name='student_home'),
-	path('browse/',views.student_browse, name='student_browse'),
-	path('domain/',views.student_domain, name='student_domain'),
-	path('lab/',views.student_lab, name='student_lab'),
+	
+	path('student/',views.student_home, name='student_home'),
+	path('student/browse/',views.student_browse, name='student_browse'),
+	path('student/domain/',views.student_domain, name='student_domain'),
+	path('student/lab/',views.student_lab, name='student_lab'),
 
 	path('prof/',views.prof_home, name='prof_home'), # recommended applications
 	path('prof/students',views.prof_students, name='prof_students'), # current students
