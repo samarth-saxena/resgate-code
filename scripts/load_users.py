c47fc0fe06af80f74bd7bfd060727f95f8805177
Samarth Saxena <saxena.samarth02@gmail.com>
2023-01-10 11:43:28 +0530

    Ceated load_users script

---
+++ scripts/load_users.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from portal.models import UserProfile, Student, Professor
+import csv
+
+
+def run():
+    # with open('csvs/labs.csv') as file:
+    #     reader = csv.reader(file)
+    #     # next(reader)  # Advance past the header
+
+    #     Lab.objects.all().delete()
+
+    #     for row in reader:
+    #         print(row)
+
+    #         _, lab = Lab.objects.get_or_create(name=row[1], website=row[2],)
+
+    #         # lab.save()
+	Student.objects.all().delete()
+	User.objects.all().exclude(username="admin").delete()
+	for i in range(1,11):
+		user = User(username="Stud"+str(i),email="stud"+str(i)+"@gmail.com",password="alpha101")
+		user.save()
+		Student.objects.get_or_create(user=user, is_Student=True)
